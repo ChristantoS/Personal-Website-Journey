2157ae708c28d093017560e60a265061cb48a737
christan.to <todotodo0420@gmail.com>
2026-03-28 16:12:39 +0700

    Changing navbar access method, improving navbar UI, try to make more content for About section

--- 02-css/practice/script.js
+++ 02-css/practice/script.js
@@ -1,3 +1,24 @@
+/*----- Section Tab Animation ----- */
+const navButtons = document.querySelectorAll('.nav-btn');
+const sections = document.querySelectorAll('.content-section')
+
+navButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const target = btn.getAttribute('data-target');
+
+        // Delete "active" class from all button and section
+        navButtons.forEach(b => b.classList.remove('active'));
+        sections.forEach(s => s.classList.remove('active'));
+
+        // Adding "active" class for selecten button and section
+        btn.classList.add('active');
+        document.getElementById(target).classList.add('active');
+    });
+});
+
+
+
+/* ----- Portfolio Card Toggle ----- */
 const allCards = document.querySelectorAll('.project-card');
 
 allCards.forEach(card => {
